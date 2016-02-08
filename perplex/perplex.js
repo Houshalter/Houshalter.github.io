65fcbc88431ab495efc5a99d60d9b0f5365ac0ff
John David Pressman <jdpostage@linuxmail.org>
2016-02-07 19:14:17 -0800

    Added DigramTable functionality.

--- perplex/perplex.js
+++ perplex/perplex.js
@@ -7,6 +7,10 @@ var score = document.getElementById('score');
 codeArea.focus();
 var text = "";
 
+var digramPeekArray = [null, null, null, null, null]
+var digramPeekTable = document.getElementById('digram-table');
+var digramTable = {}
+
 //https://api.github.com/search/repositories?q=language:JavaScript&sort=stars
 
 
@@ -34,7 +38,9 @@ function keypressed(x){
 		perplexity = total/correct;
 		score.innerHTML = perplexity.toFixed(3);
 		
-		console.log(x.charCode);
+	        console.log(x.charCode);
+	        digramPeekArray = digramTable[nextChar];
+	        update_table();
 	}
 };
 
@@ -58,7 +64,50 @@ function newLanguage(){
 			});
 		}
 	}
-	temp();
+    temp();
+    digramTable = populateDigramTable(text, {});
 }
+
+function populateDigramTable(text, digram_table) {
+    var previous = text.slice(0,1);
+    var sum = 0;
+    digram_table[previous] = {};
+    for (index = 1; index <= text.slice(1).length; index++) {
+	var character = text[index];
+	if (previous in digram_table) {
+	    if (character in digram_table[previous]) {
+		digram_table[previous][character] += 1;
+	    }
+	    else {
+		digram_table[previous][character] = 1;
+	    }
+	}
+	else {
+	    digram_table[previous] = {};
+	    digram_table[previous][character] = 1;
+	}
+	sum += 1;
+	previous = character;
+    }
+    var sorted_digram_table = {};
+    for (item in digram_table) {
+	var frequencies = []
+	for (item2 in digram_table[item]) {
+	    frequencies.push([item2, digram_table[item][item2]]);
+	}
+	frequencies.sort(compare);
+	frequencies.reverse();
+	sorted_digram_table[item] = frequencies.slice(0,6);
+    }
+    return sorted_digram_table;
+}
+
+function compare(a, b) {
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+}
+	
+	
 newLanguage();
 codeArea.onkeypress = keypressed;
