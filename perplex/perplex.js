348e31cad058eb44eb5b04c199e687e83b3c5346
Houshalter <Houshalter@gmail.com>
2016-02-10 22:33:51 -0500

    merged namespaces changes and made a ton of improvements and debugging

--- perplex/perplex.js
+++ perplex/perplex.js
@@ -10,17 +10,17 @@ var text = '';
 var correct = 0;
 var total = 0;
 var perplexity = 'Unknown!';
-lastChar = '\n';
+var lastChar = '\n';
 
 var codeArea = document.getElementById('CodeArea');
 var score = document.getElementById('score');
 codeArea.focus();
 
-var digramPeekArray = [null, null, null, null, null]
 var digramPeekTable = document.getElementById('digram-table');
-var digramTable = {}
+var digramTable = {};
 
 function keypressed(x){
+	x.preventDefault();
 	if (text.length > 0){
 		//nextChar is the next character text
 		var nextChar = text.slice(0,1);
@@ -48,11 +48,10 @@ function keypressed(x){
 		perplexity = total/correct;
 		score.innerHTML = perplexity.toFixed(3);
 		
-		//console.log(charCode);
-		digramPeekArray = digramTable[nextChar];
-		//update_table();
-		
 		lastChar = nextChar;
+		nextChar = text.slice(0,1);
+		digramPeekArray = digramTable[lastChar];
+		update_table(lastChar, digramPeekArray);
 	}
 };
 
@@ -68,7 +67,7 @@ function processDoc(text){
 	//remove carriage returns and replace tabs with spaces
 	text = text.replace(/\r/, '');
 	text = text.replace(/\t/, '    ');
-	return text
+	return text;
 }
 
 function newLanguage(){
@@ -86,44 +85,33 @@ function newLanguage(){
 		} else {
 			getRandFile(function(t){
 				text = processDoc(t);
-				codeArea.innerHTML = 'Type Here!<BR>';
+				codeArea.innerHTML = 'Type Here!\n';
+				digramTable = populateDigramTable(text, {});
 			});
 		}
 	}
     temp();
-    digramTable = populateDigramTable(text, {});
 }
 
-function populateDigramTable(text, digram_table) {
-    var previous = text.slice(0,1);
-    var sum = 0;
+function populateDigramTable(text, digram_table){
+    var previous = '\n';
     digram_table[previous] = {};
-    for (index = 1; index <= text.slice(1).length; index++) {
-	var character = text[index];
-	if (previous in digram_table) {
-	    if (character in digram_table[previous]) {
+    for (i=0;i<text.length;i++){
+		var character = text[i];
+		digram_table[previous] = digram_table[previous] || {};
+		digram_table[previous][character] = digram_table[previous][character] || 0;
 		digram_table[previous][character] += 1;
-	    }
-	    else {
-		digram_table[previous][character] = 1;
-	    }
-	}
-	else {
-	    digram_table[previous] = {};
-	    digram_table[previous][character] = 1;
-	}
-	sum += 1;
-	previous = character;
+		previous = character;
     }
     var sorted_digram_table = {};
-    for (item in digram_table) {
-	var frequencies = []
-	for (item2 in digram_table[item]) {
-	    frequencies.push([item2, digram_table[item][item2]]);
-	}
-	frequencies.sort(compare);
-	frequencies.reverse();
-	sorted_digram_table[item] = frequencies.slice(0,6);
+    for (item in digram_table){
+		var frequencies = []
+		for (item2 in digram_table[item]) {
+			frequencies.push([item2, digram_table[item][item2]]);
+		}
+		frequencies.sort(compare);
+		frequencies.reverse();
+		sorted_digram_table[item] = frequencies.slice(0,6);
     }
     return sorted_digram_table;
 }
@@ -133,7 +121,25 @@ function compare(a, b) {
     if (a[1] > b[1]) return 1;
     return 0;
 }
-	
+
+function update_table(last_char, digram_peek_array){
+	var sum = 0;
+    for (index in digram_peek_array) {
+		sum += digram_peek_array[index][1];
+    }
+	for (i=0;i<5;i++){
+		var element = document.getElementById('digram'+i);
+		var d = digram_peek_array[i];
+		if (d){
+			element.innerHTML = last_char + d[0] + ' ' + ((d[1] / sum) * 100).toFixed(0)+'%';
+		} else {
+			element.innerHTML = '';
+		}
+		
+	}
+}
+
+
 	
 newLanguage();
 codeArea.onkeypress = keypressed;
